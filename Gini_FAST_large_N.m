c3318577348e5945cc654e96bea0e15d96ded746
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2025-04-21 15:34:16 +0100

    Add files via upload

---
+++ Gini_FAST_large_N.m
@@ -0,0 +1,19 @@
+%Code for fast computation of the Gini coefficient when N is large enough
+%that bsxfun fails due to memory contraints or is otherwise slow
+%Last updated: Apr 21, 2025. Written by Michael Hatcher (m.c.hatcher@soton.ac.uk)
+
+clear, clc
+
+tic
+
+N = 50000; %No. of individuals
+Wealth_vec = linspace(realmin,1E6,N);  %Wealth distribution
+Wealth_vec = Wealth_vec';
+
+%Sort wealth data
+Wealth_norm = sort(Wealth_vec);
+
+%Fast approach to calculate Gini for large N (sorted data: https://en.wikipedia.org/wiki/Gini_coefficient)
+Gini =  (2/N)*(1:N)*Wealth_norm/sum(Wealth_norm) - (1+1/N)
+
+toc
